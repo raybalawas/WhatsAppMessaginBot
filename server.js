6eb182cfcf3485647949db5b83576a33b0ca2aee
Rahul Yadav <raybalawas@gmail.com>
2025-10-02 16:10:56 +0530

    Refactor CORS configuration to improve origin handling and update server startup log

--- server.js
+++ server.js
@@ -11,37 +11,25 @@ import WhatsAppRoutes from "./Routes/WhatsappMessageRoute.js";
 const app = e();
 app.use(e.json());
 
-// app.use(
-//   cors({
-//     // origin: "http://localhost:5173",
-//     // origin: "*", // your React app URL
-//     origin: ["https://68da870ab842ae523a1b368c--capable-bubblegum-c45fb0.netlify.app", "http://localhost:5173"],
-//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     credentials: true, // ✅ allow cookies / auth headers
-//   })
-// );
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    // origin: "http://localhost:5173",
+    // origin: "*", // your React app URL
+    origin: [
+      "https://68da870ab842ae523a1b368c--capable-bubblegum-c45fb0.netlify.app",
+      "http://localhost:5173"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
+    credentials: true, // ✅ allow cookies / auth headers
   })
 );
+
 
 // ✅ Handle OPTIONS requests
 app.options("*", cors());
 
 app.use(e.urlencoded({ extended: true }));
-// app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -52,5 +40,5 @@ const PORT = process.env.PORT ?? 5000;
 
 app.listen(PORT, () => {
   // console.log(`server is started on port:http://localhost:${PORT}`);
-  console.log(`server is started on port:${PORT}`);
+  console.log(`server is started on port:https://68da870ab842ae523a1b368c--capable-bubblegum-c45fb0.netlify.app:${PORT}`);
 });
