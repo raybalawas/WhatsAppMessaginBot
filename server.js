47c510d80b437fdb4d255a305aa4e7af876d7dd5
Rahul Yadav <raybalawas@gmail.com>
2025-10-03 15:06:59 +0530

    Remove hardcoded Netlify URL from server startup log for improved clarity

--- server.js
+++ server.js
@@ -39,5 +39,5 @@ const PORT = process.env.PORT ?? 5000;
 
 app.listen(PORT, () => {
   // console.log(`server is started on port:http://localhost:${PORT}`);
-  console.log(`server is started on port:https://68da870ab842ae523a1b368c--capable-bubblegum-c45fb0.netlify.app:${PORT}`);
+  console.log(`server is started on port:${PORT}`);
 });
